55118de6430ddf2e29f16312e0e5422d4c99c68c
Nick Papayiannakis <nikos.papayiannakis@gmail.com>
2015-10-16 19:45:51 -0400

    Added asteroids to svg canvas and music to html.

--- client/watchout.js
+++ client/watchout.js
@@ -1,2 +1,35 @@
-// start slingin' some d3 here.
+var h, w, svg, meteor, ship, totalScore, score;
 
+// Board Creation
+h = 400, w = 600;
+
+// generate initial data set
+var dataset = [
+                [1,5],
+                [34,23],
+                [67,100],
+                [100,123],
+                [200,234],
+                [500,345],
+                [149,321],
+                [345,210]
+                          ];
+
+// populate svg
+svg = d3.select('body').append('svg')
+      .attr('class', 'board')
+      .attr('height', h)
+      .attr('width', w);
+
+asteroid = svg.selectAll('circle')
+                 .data(dataset)
+                 .enter()
+                 .append('circle');
+
+asteroid.attr("cx", function(d, i){
+          return dataset[i][0];
+        })
+       .attr("cy", function(d, i){
+        return dataset[i][1];
+       })
+       .attr("r", 15);
